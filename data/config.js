b0114d6361e10590d6b1109873bda3c5e622bcd8
Little Rabbit <daddyrabbitnyc@gmail.com>
2021-06-17 16:22:56 -0400

    tweak some config values

--- data/config.js
+++ data/config.js
@@ -1,47 +1,47 @@
 module.exports = {
   // Name of your site. Can be the name of a brand, or your personal name.
-  title: 'Gatsby Starter',
+  title: 'Daddy Rabbit',
   // What's the purpose of this website? What can we expect from it?
   description:
-    `A portfolio based on your latest Instagram posts, implemented with Uber's Base Web Design System.`,
+    `Daddy Rabbit`,
   // Your legal name.
-  legalName: 'Gatsby Starter',
+  legalName: 'Daddy Rabbit',
   // URL to this website. If you bought `www.domain.com`, then it will be `www.domain.com`.
-  url: 'https://www.robonomy.com',
+  url: 'https://www.daddyrabbit.com',
   // Favicon that will display on browsers.
   logo: 'static/images/favicon.png',
   // Who made this website?
-  author: 'Juan Alejandro Morais',
+  author: 'Daddy Rabbot',
   // What's the website of the author?
-  authorUrl: 'https://www.timrodz.com',
+  authorUrl: 'https://www.daddyrabbit.com',
   // Your social media presence, in the form of usernames.
   social: {
-    // tells the Instagram API the username to get posts from. 
+    // tells the Instagram API the username to get posts from.
     // If the url is `https://www.instagram.com/natgeotravel`, your username is `natgeotravel`.
-    instagram: 'natgeotravel', 
+    instagram: 'daddyrabbit',
     // Optional
-    twitter: '@NatGeoTravel',
+    // twitter: '@NatGeoTravel',
   },
   socialLinks: {
     // Complete URL verison of `social.instagram`.
-    instagram: 'https://www.instagram.com/natgeotravel',
+    instagram: 'https://www.instagram.com/daddyrabbit',
     // Optional
-    twitter: 'https://twitter.com/NatGeoTravel',
+    // twitter: 'https://twitter.com/NatGeoTravel',
   },
   // If applicable, your Google Analytics crawl ID.
-  googleAnalyticsId: 'UA-XXXXXXXX-X',
+  // googleAnalyticsId: 'UA-XXXXXXXX-X',
   // Colours that represent your website.
   themeColor: '#000',
-  backgroundColor: '#fff',
+  backgroundColor: '#000',
   // No more information than City + Country due to privacy concerns.
   address: {
-    city: 'City',
-    country: 'Country',
+    city: 'New York',
+    country: 'USA',
   },
   // Basic contact details.
   contact: {
     // Email address to contact you.
-    email: 'timrodz@icloud.com',
+    email: 'daddyrabbitnyc@gmail.com',
   },
   // When was this website created?
   foundingDate: '2020',
